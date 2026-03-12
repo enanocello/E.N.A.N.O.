1d0fbb1459115d9743b3c576e8c64012a38c7ac7
enanocello <enanovillaloboss2004@gmail.com>
2026-03-12 20:06:35 -0300

    The adding course function is now really finished

--- interface.py
+++ interface.py
@@ -1,25 +1,27 @@
 import sqlite3
 import os
 
-def course():
-    print("Lets take the period of your course")
+def courseSemester():
+    #ASK FOR THE SEMESTER
+    print("STEP 1\nLets take the semester of your course")
     try:
         flag = True
         while flag:
             #Ask for year (Doesn't care which year lol)
-            year = int(input("Year: "))
+            year = int(input("Which year? => "))
             print(f"{year} cool year bro")
 
             if 0 <= year <= 9999:
                 flag = False
             else:
                 os.system("clear")
-                print(f"Tf you mean with {year} lol")
+                print(f"Tf you mean {year} lol")
 
         flag = True
+        os.system("clear")
         while flag:
             #Ask for a valid semester
-            semester = int(input("Semester (1 | 2): "))
+            semester = int(input("Which semester? (1 | 2) => "))
             if semester in (1,2):
                 flag = False
             else:
@@ -29,19 +31,66 @@ def course():
     except:
         #Handle error
         os.system("clear")
-        print("Dont valid option dude")
+        print("Dude I'm asking for numbers lol")
+        courseSemester()
+        exit()
 
+    os.system("clear")
     while True:
-        os.system("clear")
-        print(f"You choose {year}-{semester}\nIs it correct? (y | n)")
-        answer = input()
-        if answer == "y":
+        print(f"You choose {year}-{semester}\nIs it correct? (y | n)\nType (e) for exit")
+        answer = input("=> ").lower()
+        if answer in ("y",""):
             return(f"{year}-{semester}")
         elif answer == "n":
+            semester = courseSemester()
+            return(f"{semester}")
+        elif answer in ("e","exit"):
             return None
         else:
-            print(f"What is {answer}???")
-        
+            os.system("clear")
+            print(f"Wtf is {answer}???")
+
+def courseCode():
+    #ASK FOR THE COURSE CODE
+    os.system("clear")
+    print("STEP 2\nLets take the course code")
+    code = str(input("For example IWI-131 => ")).upper()
+    os.system("clear")
+    while True:
+        print(f"You choose {code}\nIs it correct? (y | n)\nType (e) for exit")
+        answer = str(input("=> ")).lower()
+        if answer in ("y",""):
+            return(f"{code}")
+        elif answer == "n":
+            code = courseCode()
+            return code
+        elif answer in ("e","exit"):
+            return None
+        else:
+            os.system("clear")
+            print(f"Wtf is {answer}???")
+    return
+
+def courseName():
+    #ASK FOR THE COURSE NAME
+    os.system("clear")
+    print("STEP 3\nLets take the course name")
+    name = str(input("=> ")).upper()
+    os.system("clear")
+    while True:
+        print(f"You choose {name}\nIs it correct? (y | n)\nType (e) for exit")
+        answer = input("=> ").lower()
+        if answer in ("y",""):
+            return(f"{name}")
+        elif answer == "n":
+            name = courseName()
+            return name
+        elif answer in ("e","exit"):
+            return None
+        else:
+            os.system("clear")
+            print(f"Wtf is {answer}???")
+    return
 
 def modifyMenu(cursor, conn):
     flag = True
@@ -63,13 +112,17 @@ def modifyMenu(cursor, conn):
 
     os.system("clear")
     if answer == "course":
-        newCourse = course()
-        if newCourse == None:
-            modifyMenu(cursor,conn)
-            exit()
-        else:
-            print(f"The course {newCourse} has been added succesfully compa")
-            flag = False
+        newCourseSemester = courseSemester()
+        if newCourseSemester == None: modifyMenu(cursor,conn); exit()
+        newCourseCode = courseCode()
+        if newCourseCode == None: modifyMenu(cursor,conn); exit()
+        newCourseName = courseName()
+        if newCourseName == None: modifyMenu(); exit()
+
+        os.system("clear")
+        print(f"The following course has been added succesfully bro :b\nName: {newCourseName}\nCode: {newCourseCode}\nSemester: {newCourseSemester}")
+
+        flag = False
     elif answer == "exam":
         flag = False
     else:
